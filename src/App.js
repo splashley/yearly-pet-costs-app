68e77001c56bb71e3a38ffde1e486659fa8bb6b4
splashley <ashleyjlhynes@gmail.com>
2021-01-14 22:40:54 -0500

    added changes for piechart

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Main from "./components/MainCopy";
 
 function App() {
   return (
-    <div className="bg-glacier py-10 px-32 h-screen font-quicksand">
-      <h1 className="p-10 text-6xl text-center text-white font-bold">
+    <div className="bg-glacier p-2 h-screen font-quicksand">
+      <h1 className="p-10 text-3xl text-center text-white font-bold">
         HOW MUCH WILL YOUR PET(S) ROUGHLY COST THIS YEAR?
       </h1>
       <Main />
--- src/components/MainCopy.js
+++ src/components/MainCopy.js
@@ -1,6 +1,6 @@
 import React from "react";
 import addBtn from "../assets/addBtn.png";
-// import { UserInfoContext } from "../UserInfoContext";
+import { PieChart } from "react-minimal-pie-chart";
 
 const Main = () => {
   const defaultValue = [
@@ -9,8 +9,10 @@ const Main = () => {
       monthlyAmount: "",
     },
   ];
+
   const [list, setList] = React.useState(defaultValue);
   const [runningTotal, setRunningTotal] = React.useState(0); // * get runningTotal in state
+  const [chartOptions, setChartOptions] = React.useState([]);
 
   // *
   // * Simple function to loop through the list and add up the monthlyAmount values
@@ -20,7 +22,7 @@ const Main = () => {
     let total = 0;
     list.forEach((item) => {
       if (!item.monthlyAmount) return; // * If we see a new category with an undefined monthlyAmount value, move along
-      total = total + Number(item.monthlyAmount); // * Add the monthlyAmount to the total
+      total = total + Number(item.monthlyAmount) * 12; // * Add the monthlyAmount to the total
     });
     setRunningTotal(total.toLocaleString()); // * Set the value in state so the render function can read and update it on the page
   }
@@ -33,6 +35,7 @@ const Main = () => {
     let array = [...list]; // * creates the clone of the list state so we aren't mutating state directly - big no no
     array[index].categoryName = name; // * inside your cloned state, update the categoryName using the index we passed in to our new value
     setList(array); // * after we updated our new value, update your state with the new data
+    buildChartOptions();
   }
 
   function handleAmountChange(amount, index) {
@@ -40,13 +43,38 @@ const Main = () => {
     array[index].monthlyAmount = amount; // * inside your cloned state, update the monthlyAmount using the index we passed in to our new value
     setList(array); // * after we updated our new value, update your state with the new data
     updateTotal(); // * update our totals value when an amount changes
+    buildChartOptions();
   }
 
   // * moving this logic to a separate function to clean up html
   function addEmptyCategory() {
-    const newArr = [...list, ...defaultValue]; // * create a new array using the existing list and the default value. We have to use the spread operator here on the default value because it is initialized as an array
-    setList(newArr); // * set our state with the new default value
-    updateTotal();
+    const totalCategories = list.length;
+    if (totalCategories > 10) {
+      alert("You have reached the max");
+    } else {
+      const newArr = [...list, ...defaultValue]; // * create a new array using the existing list and the default value. We have to use the spread operator here on the default value because it is initialized as an array
+      setList(newArr); // * set our state with the new default value
+      updateTotal();
+    }
+  }
+
+  function generateRandomColour() {
+    const randomColour = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomColour}`;
+  }
+
+  //  you'll need to make a function that builds that data object property with your categories array
+  function buildChartOptions() {
+    const array = [...list];
+    const optionsObject = [];
+    array.forEach((category) => {
+      optionsObject.push({
+        title: category.categoryName,
+        value: Number(category.monthlyAmount),
+        color: generateRandomColour(),
+      });
+    });
+    setChartOptions(optionsObject);
   }
 
   return (
@@ -55,6 +83,7 @@ const Main = () => {
         <div className="flex">
           <div>
             <button
+              className="align-baseline"
               onClick={() => {
                 addEmptyCategory();
               }}
@@ -76,11 +105,12 @@ const Main = () => {
             return (
               <div
                 key={index}
-                className="flex flex-row p-4 my-2 border-solid border-2 border-grey rounded-sm"
+                className="flex flex-row p-4 my-2 border-solid border-2 border-grey rounded-sm text-xs"
               >
                 <input
                   name="categoryName"
                   type="text"
+                  className="text-xs"
                   placeholder="Enter a category here"
                   value={el.categoryName}
                   onChange={(e) => {
@@ -90,19 +120,23 @@ const Main = () => {
                 <input
                   name="monthlyAmount"
                   type="number"
+                  className="relative outline-none rounded py-1 px-2 w-20 bg-white shadow text-sm text-gray-700 placeholder-gray-400 focus:outline-none focus:shadow-outline"
                   placeholder="$0"
                   value={el.monthlyAmount}
                   onChange={(e) => {
                     handleAmountChange(e.target.value, index); // * send off the value of the input and the index to our handleAmountChange function
                   }}
-                ></input>
+                ></input>{" "}
+                /monthly
               </div>
             );
           })}
         </div>
       </div>
       <div className="right-side">
-        <div id="pieChart"></div>
+        <div id="pieChart" className="p-4">
+          <PieChart data={chartOptions} />
+        </div>
         <div className="font-bold text-center ">
           TOTAL EST: ${runningTotal} PER YEAR
         </div>
